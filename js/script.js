1d4fb7f5aa56c469e4e8d418188133b724848b41
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2018-11-15 00:23:53 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('#first,.first_arrow').click(function() {
+    $('.first_list').toggleClass('is-active');
+    $('.first_arrow').toggleClass('rotate');
+    $('#first').toggleClass('is-active');
+});
+$('#second,.second_arrow').click(function() {
+    $('.second_list').toggleClass('is-active');
+    $('.second_arrow').toggleClass('rotate');
+    $('#second').toggleClass('is-active');
+});
+$('#third,.third_arrow').click(function() {
+    $('.third_list').toggleClass('is-active');
+    $('.third_arrow').toggleClass('rotate');
+    $('#third').toggleClass('is-active');
+});
+$('#fourth,.fourth_arrow').click(function() {
+    $('.fourth_list').toggleClass('is-active');
+    $('.fourth_arrow').toggleClass('rotate');
+    $('#fourth').toggleClass('is-active');
+});
+
+
+
